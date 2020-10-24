f33725695f7557983ada31b6e68c208ab79961a3
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2020-10-24 12:56:44 -0700

    Update GuestBook.java

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,7 +1,35 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	
+	public void main(String[]args) {
+	
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton();
+	JButton view = new JButton();
+	
+	add.setText("Add Name");
+	view.setText("View Names");
+	
+	add.addActionListener(this);
+	view.addActionListener(this);
+	
+	panel.add(add);
+	panel.add(view);
+	frame.add(panel);
+	frame.pack();
+	
+	frame.setVisible(true);
+	
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
@@ -9,4 +37,13 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
